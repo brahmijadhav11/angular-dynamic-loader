527bae790bae57ce16cd69087967a1e7fbe19fbc
bjadhav11 <bjadhav@xpanxion.com>
2020-06-22 22:49:17 +0530

    Demo changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DynamicLoaderService } from 'angular-dynamic-loader';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-dynamic-loader-app';
+
+  constructor(private loader: DynamicLoaderService) { }
+
+  ngOnInit() {
+  }
+
+  startLoader() {
+    /** loader starts */
+    this.loader.show();
+
+    setTimeout(() => {
+      /** loader ends after 5 seconds */
+      this.loader.hide();
+    }, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DynamicLoaderModule } from 'angular-dynamic-loader';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DynamicLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
